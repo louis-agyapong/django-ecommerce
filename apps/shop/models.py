4a02cd062d8160923e9b542a0513525784009b8e
Louis Agyapong <agyapong.louis@gmail.com>
2021-10-22 21:58:34 +0000

    product catalog models

--- apps/shop/models.py
+++ apps/shop/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.db.models import indexes
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(_("name"), max_length=255, db_index=True)
+    slug = models.SlugField(_("slug"), max_length=255, unique=True)
+
+    class Meta:
+        verbose_name = _("category")
+        verbose_name_plural = _("categories")
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        Category, related_name="products", on_delete=models.CASCADE, verbose_name=_("category")
+    )
+    name = models.CharField(_("name"), max_length=255, db_index=True)
+    slug = models.SlugField(_("slug"), max_length=255, db_index=True)
+    image = models.ImageField(_("image"), upload_to="products/%Y/%m/%d", blank=True)
+    description = models.TextField(_("description"), blank=True)
+    price = models.DecimalField(_("price"), max_digits=10, decimal_places=2)
+    available = models.BooleanField(_("available"), default=True)
+    created = models.DateTimeField(_("created"), auto_now_add=True)
+    updated = models.DateTimeField(_("updated"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("product")
+        verbose_name_plural = _("products")
+        ordering = ("name",)
+        index_together = (("id", "slug"),)
+
+    def __str__(self):
+        return self.name
