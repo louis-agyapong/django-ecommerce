f53b0c22ea60d98c9938e38b973e5d3938f17ccb
Louis Agyapong <agyapong.louis@gmail.com>
2022-03-09 23:16:25 +0000

    ecommerce category model

--- apps/ecommerce/models.py
+++ apps/ecommerce/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from mptt.models import MPTTModel, TreeForeignKey, TreeManyToManyField
 
-# Create your models here.
+
+class Category(MPTTModel):
+    """
+    Inventory category table implemented with MPTT
+    """
+
+    name = models.CharField(_("Name"), max_length=100)
+    slug = models.SlugField(_("Slug"), max_length=100, help_text=_("A URL-friendly title."))
+    is_active = models.BooleanField(_("Is active"), default=True)
+    parent = TreeForeignKey(
+        "self",
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+        related_name="children",
+        verbose_name=_("Parent"),
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ["name"]
+
+    class Meta:
+        verbose_name = _("product Category")
+        verbose_name_plural = _("product categories")
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
